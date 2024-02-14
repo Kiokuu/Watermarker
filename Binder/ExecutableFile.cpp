89c3697b98973a215b213faffee51b38f2fd5f89
LiteralPotatoDeveloper <91877647+LiteralPotatoDeveloper@users.noreply.github.com>
2024-02-14 11:42:16 +0000

    Binder: ExecutableFile error checking

--- Binder/ExecutableFile.cpp
+++ Binder/ExecutableFile.cpp
@@ -44,16 +44,45 @@ void ExecutableFile::parse()
 void ExecutableFile::parseDosHeader()
 {
 	m_dosHeader = *reinterpret_cast<IMAGE_DOS_HEADER*>(m_data.data());
+
+	if (m_dosHeader.e_magic != IMAGE_DOS_SIGNATURE)
+	{
+		throw std::runtime_error("Invalid DOS header");
+	}
+
+	if (m_dosHeader.e_lfanew == 0 || m_dosHeader.e_lfanew >= m_data.size())
+	{
+		throw std::runtime_error("Invalid DOS header -- NT Header offset");
+	}
 }
 
 void ExecutableFile::parseNtHeaders()
 {
 	m_ntHeaders = *reinterpret_cast<IMAGE_NT_HEADERS*>(m_data.data() + m_dosHeader.e_lfanew);
+	if (m_ntHeaders.Signature != IMAGE_NT_SIGNATURE)
+	{
+		throw std::runtime_error("Invalid NT headers");
+	}
+
+	if (m_ntHeaders.FileHeader.SizeOfOptionalHeader != sizeof(IMAGE_OPTIONAL_HEADER))
+	{
+		throw std::runtime_error("Invalid optional header size");
+	}
+
+	if (m_ntHeaders.OptionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR64_MAGIC) // 64bit
+	{
+		throw std::runtime_error("Invalid optional header magic");
+	}
+
+	if (m_ntHeaders.OptionalHeader.AddressOfEntryPoint == 0)
+	{
+		throw std::runtime_error("Invalid entry point");
+	}
 }
 
 void ExecutableFile::parseSections()
 {
-	for (size_t i = 0; i< m_ntHeaders.FileHeader.NumberOfSections; i++)
+	for (size_t i = 0; i < m_ntHeaders.FileHeader.NumberOfSections; i++)
 	{
 		const auto section = reinterpret_cast<IMAGE_SECTION_HEADER*>(m_data.data() + m_dosHeader.e_lfanew + sizeof(
 			IMAGE_NT_HEADERS) + i * sizeof(IMAGE_SECTION_HEADER));
