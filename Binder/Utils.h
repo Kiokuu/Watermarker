f6c143195db30e70be231569dd37c47d5f077182
Thomas Savine <91877647+LiteralPotatoDeveloper@users.noreply.github.com>
2024-02-15 21:31:47 +0000

    Binder: Add wstring/string conversion.

--- Binder/Utils.h
+++ Binder/Utils.h
@@ -7,4 +7,30 @@ namespace Binder
 	{
 		return (value + alignment - 1) & ~(alignment - 1);
 	}
+
+	inline std::wstring convertToWChar(const char* src)
+	{
+		const size_t size = strlen(src) + 1;
+
+		wchar_t* dest = new wchar_t[size];
+		size_t outSize;
+		errno_t err = mbstowcs_s(&outSize, dest, size, src, size - 1);
+		std::wstring result(dest);
+		delete[] dest;
+
+		return result;
+	}
+
+	inline std::string convertToChar(const wchar_t* src)
+	{
+		const size_t size = wcslen(src) + 1;
+
+		char* dest = new char[size];
+		size_t outSize;
+		errno_t err = wcstombs_s(&outSize, dest, size, src, size - 1);
+		std::string result(dest);
+		delete[] dest;
+
+		return result;
+	}
 }
