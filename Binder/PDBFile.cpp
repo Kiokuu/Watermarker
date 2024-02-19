a2a12a4dc9f9e9ab063ee6431c4cb30a39281990
Thomas Savine <91877647+LiteralPotatoDeveloper@users.noreply.github.com>
2024-02-19 16:40:41 +0000

    Binder: PDBFile refactor, mapping symbols

--- Binder/PDBFile.cpp
+++ Binder/PDBFile.cpp
@@ -2,72 +2,90 @@
 #include "utils.h"
 #include "ExecutableFile.h"
 
+#include <comutil.h>
+#include <comdef.h>
 #include <fstream>
 #include <atlbase.h>
 #include <iostream>
 #include <array>
-#include <dia2.h>
-#include <DbgHelp.h>
 #include <regex>
 
+
+static int EXE_COUNT = 1;
 
 const std::regex nameRegex = std::regex("[^a-zA-Z0-9_]");
 
 PDBFile::PDBFile(ExecutableFile* executableFile, std::string_view path) :
 	m_executableFile(executableFile)
 {
-	// Initialize COM
+	std::cout << "\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+	std::cout << "PDBFile: " << EXE_COUNT++ << "\n";
+	std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+
+	initializeCOM();
+	loadDataFromPDB(path);
+	openSession();
+	getGlobalScope();
+	processSymbols();
+}
+
+void PDBFile::initializeCOM()
+{
 	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to initialize COM");
 	}
+}
 
-
-	CComPtr<IDiaDataSource> pSource;
-	hr = CoCreateInstance(CLSID_DiaSource,
-	                      nullptr,
-	                      CLSCTX_INPROC_SERVER,
-	                      __uuidof(IDiaDataSource),
-	                      (void**)&pSource);
+void PDBFile::loadDataFromPDB(std::string_view path)
+{
+	HRESULT hr = CoCreateInstance(CLSID_DiaSource,
+                               nullptr,
+                               CLSCTX_INPROC_SERVER,
+                               __uuidof(IDiaDataSource),
+                               (void**)&m_pSource);
 
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to create DIA source");
 	}
 
-
-	hr = pSource->loadDataFromPdb(Binder::convertToWChar(path.data()).c_str());
+	hr = m_pSource->loadDataFromPdb(Binder::convertToWChar(path.data()).c_str());
 
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to load PDB file");
 	}
+}
 
-	// Open a session for querying symbols.
-	CComPtr<IDiaSession> pSession;
-	hr = pSource->openSession(&pSession);
+void PDBFile::openSession()
+{
+	HRESULT hr = m_pSource->openSession(&m_pSession);
 
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to open session");
 	}
+}
 
-	// Print the number of symbols in the program database.
-	CComPtr<IDiaSymbol> pGlobal;
-	hr = pSession->get_globalScope(&pGlobal);
+void PDBFile::getGlobalScope()
+{
+	HRESULT hr = m_pSession->get_globalScope(&m_pGlobal);
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to get global scope");
 	}
+}
 
-
-	constexpr std::array symEnums{SymTagFunction, SymTagLabel, SymTagPublicSymbol};
+void PDBFile::processSymbols()
+{
+	constexpr std::array symEnums{SymTagFunction, SymTagData, SymTagLabel, SymTagPublicSymbol};
 
 	for (const auto symEnum : symEnums)
 	{
 		CComPtr<IDiaEnumSymbols> pEnumSymbols;
-		hr = pGlobal->findChildren(symEnum, nullptr, nsNone, &pEnumSymbols);
+		HRESULT hr = m_pGlobal->findChildren(symEnum, nullptr, nsNone, &pEnumSymbols);
 		if (FAILED(hr))
 		{
 			throw std::runtime_error("Failed to find children");
@@ -77,34 +95,140 @@ PDBFile::PDBFile(ExecutableFile* executableFile, std::string_view path) :
 		CComPtr<IDiaSymbol> pSymbol;
 		while (SUCCEEDED(pEnumSymbols->Next(1, &pSymbol, &celt)) && celt == 1)
 		{
-			DWORD rva;
-			pSymbol->get_relativeVirtualAddress(&rva);
-
-			if (rva == 0)
-			{
-				pSymbol.Release();
-				continue;
-			}
-
-			BSTR name;
-			pSymbol->get_name(&name);
-
-			// Undecorate the symbol name
-			wchar_t m_symbolBuffer[1024];
-			UnDecorateSymbolNameW(name, m_symbolBuffer, std::size<wchar_t>(m_symbolBuffer), UNDNAME_NAME_ONLY);
-
-			std::string symbolName = Binder::convertToChar(m_symbolBuffer);
-			std::string filteredName = std::regex_replace(symbolName, nameRegex, "") + "_" + std::to_string(
-				m_executableFile->getHash() * rva); // Add hash to avoid name collisions
-
-			std::cout << "Filtered Name: " << filteredName << '\n';
-			std::cout << "RVA: 0x" << std::hex << rva << "  " << symbolName << '\n';
-
-			m_symbols[rva] = filteredName;
-
+			processSymbol(pSymbol);
 			pSymbol.Release();
-			SysFreeString(name);
 			celt = 0;
 		}
+	}
+}
+
+void PDBFile::processSymbol(const CComPtr<IDiaSymbol>& pSymbol)
+{
+	DWORD rva;
+	pSymbol->get_relativeVirtualAddress(&rva);
+
+	if (rva == 0)
+	{
+		return;
+	}
+
+	BSTR name;
+	pSymbol->get_name(&name);
+
+
+	// Undecorate the symbol name
+	wchar_t m_symbolBuffer[1024];
+	UnDecorateSymbolNameW(name, m_symbolBuffer, std::size<wchar_t>(m_symbolBuffer), UNDNAME_NAME_ONLY);
+
+	std::string symbolName = Binder::convertToChar(m_symbolBuffer);
+	std::string filteredName = std::regex_replace(symbolName, nameRegex, "") + "_" + std::to_string(
+		m_executableFile->getHash() * rva); // Add hash to avoid name collisions
+
+	
+
+
+	uint64_t len = getLength(pSymbol);
+	DWORD type = 0;
+	//pSymbol->get_length(&len);
+
+	// Get the type of the symbol
+	DWORD symTag;
+	pSymbol->get_symTag(&symTag);
+
+	// things can be public symbol, how does this affect it...?
+
+	// look into get_function/get_code.
+	// table of enums
+
+
+
+	std::cout << "Symbol: " << filteredName << " (0x" << std::hex << rva << ")" << std::dec << " (len: 0x" << std::hex << len << std::dec << ")" << " (type: " << symTag << ")" << "\n";
+
+	BOOL isFunction = false;
+
+	switch(symTag)
+	{
+		case SymTagFunction:
+			m_symbols[rva] = {filteredName, len, SymbolType::Function};
+			break;
+		case SymTagData:
+			m_symbols[rva] = {filteredName, len, SymbolType::Data};
+			break;
+		case SymTagPublicSymbol:
+			if(m_symbols.contains(rva))
+			{
+				m_symbols[rva].setPublic(true);
+			}
+			else
+			{
+				// It may be a function or code...?
+				pSymbol->get_function(&isFunction);
+				if(isFunction)
+				{
+					m_symbols[rva] = {filteredName, len, SymbolType::Function, true};
+				}
+				else
+				{
+					m_symbols[rva] = {filteredName, len, SymbolType::Data, true};
+				}
+			}
+			break;
+		case SymTagLabel:
+			m_symbols[rva] = {filteredName, len, SymbolType::Label};
+			break;
+		case SymTagCompiland:
+			break;
+		default:
+			break;
+	}
+
+	SysFreeString(name);
+}
+
+size_t PDBFile::getLength(const CComPtr<IDiaSymbol>& pSymbol)
+{
+	DWORD symTag;
+	pSymbol->get_symTag(&symTag);
+	uint64_t len = 0;
+
+	if(symTag == SymTagData)
+	{
+		IDiaSymbol* pType;
+
+		if(SUCCEEDED(pSymbol->get_type(&pType)))
+		{
+			pType->get_length(&len);
+		}
+		else
+		{
+			if(FAILED(pSymbol->get_length(&len)))
+			{
+				len = 0;
+			}
+		}
+		
+		pType->Release();
+	}
+	else
+	{
+		if(FAILED(pSymbol->get_length(&len)))
+		{
+			len = 0;
+		}
+	}
+
+	return len;
+}
+
+bool PDBFile::getSymbol(uint64_t address, Symbol* outSymbol) const
+{
+	if(m_symbols.contains(address))
+	{
+		*outSymbol = m_symbols.at(address);
+		return true;
+	}
+	else
+	{
+		return false;
 	}
 }
--- Binder/PDBFile.h
+++ Binder/PDBFile.h
@@ -1,10 +1,46 @@
 #pragma once
 #include <string_view>
 #include <map>
-
+#include <dia2.h>
+#include <DbgHelp.h>
+#include <atlbase.h>
+#include <unordered_map>
 
 // Forward declare executable file
 class ExecutableFile;
+
+
+enum class SymbolType
+{
+	Function,
+	Variable,
+	PublicSymbol,
+	Data,
+	Label
+};
+
+class Symbol
+{
+public:
+	Symbol() = default;
+	Symbol(std::string_view name, size_t length, SymbolType type, bool isPublic = false)
+		: m_name(name), m_length(length), m_type(type), m_isPublic(isPublic)
+	{
+	}
+
+	std::string getName() const { return m_name; }
+	size_t getLength() const { return m_length; }
+	SymbolType getType() const { return m_type; }
+
+	void setPublic(bool isPublic) { m_isPublic = isPublic; }
+	void setLength(size_t length) { m_length = length; }
+
+private:
+	std::string m_name;
+	size_t m_length;
+	SymbolType m_type;
+	bool m_isPublic;
+};
 
 
 class PDBFile
@@ -13,11 +49,35 @@ public:
 	PDBFile() = default;
 	PDBFile(ExecutableFile* executableFile, std::string_view path);
 
-	std::string getSymbol(uint64_t address) const;
+	//~PDBFile();
+
+	void initializeCOM();
+	void loadDataFromPDB(std::string_view path);
+	void openSession();
+	void getGlobalScope();
+	void processSymbols();
+
+	void processSymbol(const CComPtr<IDiaSymbol>& pSymbol);
+
+
+
+	DWORD getType(const CComPtr<IDiaSymbol>& pSymbol);
+	size_t getLength(const CComPtr<IDiaSymbol>& pSymbol);
+
+
+	bool getSymbol(uint64_t address, Symbol* outSymbol) const;
+	const std::unordered_map<uint64_t, Symbol>& getSymbols() const { return m_symbols; }
+
 
 	ExecutableFile* getExecutableFile() const { return m_executableFile; }
 
 private:
 	ExecutableFile* m_executableFile;
-	std::map<uint64_t, std::string> m_symbols;
+	std::unordered_map<uint64_t, Symbol> m_symbols;
+
+
+	CComPtr<IDiaDataSource> m_pSource;
+	CComPtr<IDiaSession> m_pSession;
+	CComPtr<IDiaSymbol> m_pGlobal;
+
 };
