1c8c159150026c37b3b51784db0fc38d748b27bd
Thomas Savine <91877647+LiteralPotatoDeveloper@users.noreply.github.com>
2024-02-26 15:58:05 +0000

    Binder: Call watermarks export.

--- Binder/main.cpp
+++ Binder/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
 		std::cout << "arg[" << i << "]: " << argv[i] << "\n";
 	}
 
+	/*
 	if (argc < 4)
 	{
 		std::cerr << "Usage: " << argv[0] << " <first_path> " << "<first_pdb> " << "<second_path> " << "<second_pdb>" <<
@@ -29,13 +30,57 @@ int main(int argc, char* argv[])
 	ExecutableFile targetExecutable(targetPath, targetPDBPath);
 
 	Binder binder(&originalExecutable, &targetExecutable);
+	*/
 
-	/*
 	zasm::Program program(zasm::MachineMode::AMD64);
 	zasm::x86::Assembler assembler(program);
 	zasm::Serializer serializer;
 
 	ExecutableFile file(argv[1]);
+	file.addImport("Watermark.dll", "?Watermark@@YAXXZ");
+	file.rewriteImports();
+
+	ImageSection* newSection;
+	file.createSection(".newsec", 0x1000, IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_WRITE | IMAGE_SCN_MEM_EXECUTE, &newSection);
+
+	uint32_t OEP = file.getEntryPoint();
+	uint32_t waterAddress = file.getImportAddress("Watermark.dll", "?Watermark@@YAXXZ");
+
+	auto startLabel = program.createLabel("start");
+	assembler.bind(startLabel);
+
+	std::vector<zasm::x86::Gp> regsToSave = { zasm::x86::rcx, zasm::x86::rdx, zasm::x86::r8, zasm::x86::r9, zasm::x86::rbp };
+	for(auto reg : regsToSave)
+	{
+		assembler.push(reg);
+	}
+
+
+	assembler.mov(zasm::x86::rbp, zasm::x86::rsp);
+
+	assembler.call(zasm::x86::qword_ptr(zasm::x86::rip, waterAddress));
+
+	assembler.mov(zasm::x86::rsp, zasm::x86::rbp);
+
+	for(auto reg : regsToSave)
+	{
+		assembler.pop(reg);
+	}
+
+	assembler.jmp(zasm::Imm32(OEP));
+
+	serializer.serialize(program, newSection->getVirtualAddress());
+	std::uint8_t* code = const_cast<uint8_t*>(serializer.getCode());
+	const std::size_t codeSize = serializer.getCodeSize();
+
+	newSection->setData({code, codeSize});
+
+	// Set the entry point of the executable file
+	file.setEntryPoint(newSection->getVirtualAddress());
+
+	file.save("modified.exe");
+
+	/*
 	file.addImport("USER32.dll", "MessageBoxA");
 	file.rewriteImports();
 
